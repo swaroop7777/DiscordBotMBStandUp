c354f8a910ebac9e3c101001a55cbb24e4741c8f
suvam <suvam123456@gmail.com>
2021-01-05 18:17:38 +0530

    schedule with day

--- src/main/java/com/suvam/discord_Stand_up_bot/repository/QuestionRepository.java
+++ src/main/java/com/suvam/discord_Stand_up_bot/repository/QuestionRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface QuestionRepository extends JpaRepository<StandUpQuestion, Integer> {
 
-    @Query(value = "SELECT * FROM questions WHERE question=?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM questions WHERE question=?1 LIMIT 1", nativeQuery = true)
     public StandUpQuestion getStandUpQuestionByquestion(String question);
 }
